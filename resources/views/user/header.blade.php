5e1536e8b40365eed2f521bb06d82d8b246e2ef0
eto <eto.saviour80@gmail.com>
2025-10-24 14:45:47 +0100

    update

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -880,10 +880,10 @@
 }
 </style>
 
-<!-- Smartsupp Live Chat script -->
+<<!-- Smartsupp Live Chat script -->
 <script type="text/javascript">
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = '36894bd3258b8c93e942983fe3065649af6464a7';
+_smartsupp.key = '23953927cb41ee739722ce20ed7c44829348837d';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
@@ -893,4 +893,3 @@ window.smartsupp||(function(d) {
 </script>
 <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
- 
